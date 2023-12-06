74c5a777a538ba3f9c3c82b18963cc6a00bac0f7
amac81 <arnaldocanelas@gmail.com>
2023-12-06 10:43:34 +0000

    Classe TaxPayer implementada

---
+++ src/entities/TaxPayer.java
@@ -0,0 +1,88 @@
+package entities;
+
+public class TaxPayer {
+	private double salaryIncome;
+	private double servicesIncome;
+	private double capitalIncome;
+	private double healthSpending;
+	private double educationSpending;
+	
+	public TaxPayer() {}
+
+	public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending,
+			double educationSpending) {
+		this.salaryIncome = salaryIncome;
+		this.servicesIncome = servicesIncome;
+		this.capitalIncome = capitalIncome;
+		this.healthSpending = healthSpending;
+		this.educationSpending = educationSpending;
+	}
+
+	public double getSalaryIncome() {
+		return salaryIncome;
+	}
+
+	public void setSalaryIncome(double salaryIncome) {
+		this.salaryIncome = salaryIncome;
+	}
+
+	public double getServicesIncome() {
+		return servicesIncome;
+	}
+
+	public void setServicesIncome(double servicesIncome) {
+		this.servicesIncome = servicesIncome;
+	}
+
+	public double getCapitalIncome() {
+		return capitalIncome;
+	}
+
+	public void setCapitalIncome(double capitalIncome) {
+		this.capitalIncome = capitalIncome;
+	}
+
+	public double getHealthSpending() {
+		return healthSpending;
+	}
+
+	public void setHealthSpending(double healthSpending) {
+		this.healthSpending = healthSpending;
+	}
+
+	public double getEducationSpending() {
+		return educationSpending;
+	}
+
+	public void setEducationSpending(double educationSpending) {
+		this.educationSpending = educationSpending;
+	}
+	
+	public double salaryTax() {
+		return 0.0;
+	}
+	
+	public double servicesTax() {
+		return 0.0;
+	}
+	
+	public double capitalTax() {
+		return 0.0;
+	}
+	
+	public double grossTax() {
+		return 0.0;
+	}
+	
+	public double taxRebate() {
+		return 0.0;
+	}
+	
+	public double netTax() {
+		return 0.0;
+	}
+	
+	
+	
+
+}
